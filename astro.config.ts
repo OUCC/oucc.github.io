fbeaed657353692f63b68a8851c60d772c810da9
Ciffelia <mc.prince.0203@gmail.com>
2023-03-02 16:08:41 +0900

    Initial commit (WIP)

---
+++ astro.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import prefetch from '@astrojs/prefetch'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://oucc.org',
+  integrations: [
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+    tailwind(),
+    sitemap(),
+    prefetch({ selector: 'a' }),
+  ],
+})
