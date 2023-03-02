ad769c9010e17f1d2a4d77d48ecc6283a998e4cc
Ciffelia <mc.prince.0203@gmail.com>
2023-03-02 21:09:29 +0900

    ボタンの影を調整

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,16 @@ module.exports = {
       colors: {
         primary: '#1a2872',
         secondary: '#f1f2f7',
-        discord: '#5865F2',
+        discord: '#5865f2',
       },
       dropShadow: {
         contrast: '0 2px 2px rgba(0, 0, 0, 0.25)',
+        'primary-light': '0 4px 8px #1a2872a0',
+        'primary-heavy': '0 4px 8px #1a2872e0',
+        'white-light': '0 4px 8px #ffffffa0',
+        'white-heavy': '0 4px 8px #ffffffe0',
+        'discord-light': '0 4px 8px #5865f2a0',
+        'discord-heavy': '0 4px 8px #5865f2e0',
       },
     },
   },
