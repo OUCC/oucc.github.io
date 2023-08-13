fca48a918d593a288aba70513981cc5159db186f
Ciffelia <mc.prince.0203@gmail.com>
2023-08-13 20:09:40 +0900

    Update prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,13 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  trailingComma: 'all',
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 }
