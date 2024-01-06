fa77ed9ee621c9a8d2d38ec6715896c1c70358e0
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2024-01-06 10:14:18 +0900

    rlcの型を追加

--- src/@types/remark-link-card.ts
+++ src/@types/remark-link-card.ts
@@ -1,6 +1,22 @@
 // ./node_modules/remark-link-card/index.js
 
 declare module 'remark-link-card' {
-  const rlc: (options: any) => any
+  /** https://github.com/gladevise/remark-link-card?tab=readme-ov-file#api */
+  export interface RemarkLinkCardOptions {
+    /**
+     * Cache image for [`next/image`](https://nextjs.org/docs/api-reference/next/image) (`bool`, default:`false`)
+     *
+     * Automatically save open graph images and favicon images to `process.cwd()/public/remark-link-card/`.
+     *
+     * Automatically insert the path to images starting with `/remark-link-card/` in the src attribute of img tags.
+     */
+    cache?: boolean
+    /**
+     * Display only hostname of target URL (`bool`, default: `false`)
+     */
+    shortenUrl?: boolean
+  }
+
+  const rlc: (options?: RemarkLinkCardOptions) => (tree: any) => Promise<any>
   export default rlc
 }
