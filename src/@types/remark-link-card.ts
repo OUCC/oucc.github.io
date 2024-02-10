35245dc4cba03c170a26458df1772e9e912c52ce
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2024-02-10 22:57:16 +0900

    #87 不要な変更を削除

--- src/@types/remark-link-card.ts
+++ src/@types/remark-link-card.ts
@@ -1,22 +1,6 @@
 // ./node_modules/remark-link-card/index.js
 
 declare module 'remark-link-card' {
-  /** https://github.com/gladevise/remark-link-card?tab=readme-ov-file#api */
-  export interface RemarkLinkCardOptions {
-    /**
-     * Cache image for [`next/image`](https://nextjs.org/docs/api-reference/next/image) (`bool`, default:`false`)
-     *
-     * Automatically save open graph images and favicon images to `process.cwd()/public/remark-link-card/`.
-     *
-     * Automatically insert the path to images starting with `/remark-link-card/` in the src attribute of img tags.
-     */
-    cache?: boolean
-    /**
-     * Display only hostname of target URL (`bool`, default: `false`)
-     */
-    shortenUrl?: boolean
-  }
-
-  const rlc: (options?: RemarkLinkCardOptions) => (tree: any) => Promise<any>
+  const rlc: (options: any) => any
   export default rlc
 }
