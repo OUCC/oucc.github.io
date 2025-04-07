8acdd9813e3dee92ee41353953d0d7481ea4f54a
TakenPt <takekimu14@gmail.com>
2025-04-07 09:36:08 +0900

    講習会情報に主催団体を追加

---
+++ src/@types/ou-techs-members.ts
@@ -0,0 +1,12 @@
+/**
+ * @description OUTechsに参加している団体を表す型
+ */
+export type OUTechsCirclesName =
+  | 'OUCC'
+  | 'ToolBox'
+  | 'RAINBOU'
+  | '再履バス同好会 技術部'
+  | 'GDG on Campus Osaka'
+  | 'GGC'
+  | 'Wani Hackase'
+  | 'Robohan'
